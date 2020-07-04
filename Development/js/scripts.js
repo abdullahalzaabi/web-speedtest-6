3bab4774e634bd9452afd7e7cdce825176e48fc1
abdullahalzaabi <66646611+abdullahalzaabi@users.noreply.github.com>
2020-07-04 17:24:08 +0300

    ...

--- Development/js/scripts.js
+++ Development/js/scripts.js
@@ -1,17 +1,37 @@
 // قم بإنشاء الدوال التالية
 
 /*
+
  * greet(name) اسم الدالة
  * - name قد تستقبل وقد لا تستقبل
  * - name إن لم تستقبل اسم أو "Hello" تطبع
  * - name إن استقبلت اسم أو "Hello <name>" تطبع
  */
 
-function greet(name) {}
+function greet(name) {
+let message = "hello";
+if (name) {
+message+=name;
 
+}
+console.log (message);
+}
 /*
  * isOdd(n) اسم الدالة
  * - n تستقبل رقم
  * - عدا ذلك false إن كان الرقم فردياً وترجع true ترجع القيمة
  */
-function isOdd(n) {}
+function isOdd(n) {
+if (n % 2 == 1) {
+return true;
+}
+else {
+return false
+
+}
+
+
+
+
+
+
